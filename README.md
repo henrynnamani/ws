4239d85be32cac0a7f9db14c1eb54bcd103e6a88
gr4yx01 <hoyx0101@gmail.com>
2025-06-07 23:21:35 +0100

    feat: done

--- README.md
+++
@@ -1,1 +0,0 @@
-Double down into Socket.io
--- socket-intro/index.js
+++ socket-intro/index.js
@@ -1,1 +1,15 @@
 const express = require('express')
+const app = express()
+const socketio = require('socket.io')
+
+app.use(express.static(__dirname + '/public'))
+
+const server  = app.listen(8000)
+
+const io = socketio(server)
+
+io.on('connection', (socket) => {
+    console.log(`${socket.id} has connected`)
+
+    socket.emit('messageFromServer', { data: 'Serving is sending its hello greetings'})
+})
